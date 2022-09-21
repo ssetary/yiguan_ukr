321e938eaefd39ead722023523111eacbb5b21fe
ssetary <set.vusatiuk@gmail.com>
2022-09-21 13:53:35 +0000

    added new value forma

---
+++ js/contact-form.js
@@ -0,0 +1,20 @@
+// $('input[name="contacts__name"').on('change', onChange);     
+// $('input[name="contacts__phone"]').on('change', onChange);
+// $('input[name="contacts__email"').on('change', onChange);     
+// $('input[name="contacts__bio"]').on('change', onChange);
+
+// function onChange () {
+//   var nameVal = $('input[name="start"]').val();
+//   var phoneVal = $('input[name="end"]').val();
+//   var emailVal = $('input[name="start"]').val();
+//   var bioVal = $('input[name="end"]').val();
+//   alert( nameVal + phoneVal + emailVal + bioVal);
+// }
+
+
+
+
+function contacts__formFields() {
+    var values = $(".contacts__form_fields").val();
+    alert(values);
+  }
